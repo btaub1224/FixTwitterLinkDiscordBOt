950ded15feb4161f19b296fc376b2fb991a9f0e4
Ben Taub <64754027+btaub1224@users.noreply.github.com>
2024-01-30 14:31:32 +0200

    Fix Pylint issues

--- ftl.py
+++ ftl.py
@@ -1,5 +1,8 @@
-import discord
+"""
+Discord bot to fix Twitter/Tiktok links so media displays appropiately
+"""
 import os
+import discord
 from dotenv import load_dotenv
 intents = discord.Intents.all()
 
@@ -8,10 +11,16 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+"""
+Show bot is logged in 
+"""
     print("Logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
+"""
+Check every message (not sent by the bot) and check for valid urls to change
+"""
     if message.author == client.user:
         return
 
@@ -19,12 +28,16 @@ async def on_message(message):
     tiktok_url = "https://www.tiktok.com"
 
     if any(valid_url in message.content for valid_url in valid_urls):
-        new_message = "https://fxtwitter.com" + str(message.content.split(".com", 1)[1].split(" ", 1)[0])
-        await message.channel.send("Fixed twitter link, originally posted by posted by " + str(message.author) + ": " + new_message)
+        new_message = "https://fxtwitter.com" + 
+            str(message.content.split(".com", 1)[1].split(" ", 1)[0])
+        await message.channel.send("Fixed twitter link, originally posted by posted by " + 
+            str(message.author) + ": " + new_message)
 
     elif tiktok_url in message.content:
-        new_message = "https://vxtiktok.com" + str(message.content.split(".com", 1)[1].split(" ", 1)[0])
-        await message.channel.send("Fixed tiktok link, originally posted by posted by " + str(message.author) + ": " + new_message)
+        new_message = "https://vxtiktok.com" + 
+            str(message.content.split(".com", 1)[1].split(" ", 1)[0])
+        await message.channel.send("Fixed tiktok link, originally posted by posted by " + 
+            str(message.author) + ": " + new_message)
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
